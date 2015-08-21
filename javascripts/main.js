a7fd93543ac08c13f88b7d9f40cf78e00dcdf0b3
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-21 11:01:51 -0500

    basic zipcode input

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,12 @@
 requirejs.config({
   baseUrl: './javascripts',
   paths: {
-    'jquery': '../bower_components/jquery/dist/jquery.min',
-    'firebase': '../bower_components/firebase/firebase',
-    'lodash': '../bower_components/lodash/lodash.min',
-    'hbs': '../bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
-    'q': '../bower_components/q/q'
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'firebase': '../lib/bower_components/firebase/firebase',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q'
   },
   shim: {
     'bootstrap': ['jquery'],
@@ -20,4 +20,23 @@ requirejs.config({
 // The main function requiring all our anciliary scripts
 requirejs(["jquery", "lodash", "firebase", "hbs", "bootstrap", "q"], 
   function($, _, _firebase, Handlebars, bootstrap, Q){
+
+
+/////////////////////////////
+  $('#submitButton').on("click", function(){
+    var zipInput = $('#zipInput').val();
+    console.log(zipInput);
+    if(zipInput.length === 5){
+      console.log("yes");
+    }
+    else{
+      console.log("no");
+      $('#message').html("Not zipcode. Please try again.");
+    }
+  });
+
+
+
+
+//////////////////////
 });
