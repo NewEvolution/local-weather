d0fd42ccad50af415cdf4322a3960b430aba21dd
Ryan Tanay <ryantanay@newevolution.org>
2015-08-21 10:25:05 -0500

    Initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,23 @@
+// Pull in all the various javascript libraries
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../bower_components/jquery/dist/jquery.min',
+    'firebase': '../bower_components/firebase/firebase',
+    'lodash': '../bower_components/lodash/lodash.min',
+    'hbs': '../bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../bower_components/q/q'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
+});
+
+// The main function requiring all our anciliary scripts
+requirejs(["jquery", "lodash", "firebase", "hbs", "bootstrap", "q"], 
+  function($, _, _firebase, Handlebars, bootstrap, Q){
+});
