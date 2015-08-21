ae3ff33e694a623c15f8590d65d798f25b9718c2
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-21 14:56:42 -0500

    required template in forecastAPI

--- javascripts/forecastAPI.js
+++ javascripts/forecastAPI.js
@@ -3,19 +3,22 @@ define(["jquery"],function($) {
     
     //function to pull from weather app  //
     getForecast: function(cityName, numberDays) {
+      var template=require("hbs!../templates/displayForecast");
+///////////////////////////
+
+
       console.log("pulling from api");
       $.ajax({
         url: "http://api.openweathermap.org/data/2.5/forecast?q="+cityName+",us&units=imperial&cnt="+numberDays,
         method: "GET"
 
-        }).done(function(data) {
- 
-          console.log("data from cityName and numberDays", data);
-         
+      }).done(function(data) {
+        console.log("data from cityName and numberDays", data);
+        $("#forecastTarget").html(template(data));
       }).fail(function(xhr, status, error){
         console.log(error);
       });
-          return data;
+      return data;
     }   
  };
 });
